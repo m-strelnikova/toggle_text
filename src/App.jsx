80c21c49895d682cfc2bc12809f72fc927611e1d
MStrelnikova <hashitomada@gmail.com>
2026-07-16 21:28:13 +0200

    setup

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css';
+import ToggleText from './components/toggleText';
+
+function App() {
+  
+
+  return (
+    <div>
+      <ToggleText />
+    </div>
+  )
+}
+
+export default App
